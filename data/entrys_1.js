01d75c09f1a9ea50855d5b3b0483c8caac492121
davidels1 <87242587+davidels1@users.noreply.github.com>
2021-07-16 17:24:49 +0200

    Add files via upload

---
+++ data/entrys_1.js
@@ -0,0 +1,19 @@
+var json_entrys_1 = {
+"type": "FeatureCollection",
+"name": "entrys_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Entrance 1\n", "descriptio": "1", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.338, "xx": "27.4164563", "yy": "-26.3377197" }, "geometry": { "type": "Point", "coordinates": [ 27.4164563, -26.3377197 ] } },
+{ "type": "Feature", "properties": { "Name": "Entrance 2\n", "descriptio": "2", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.344, "xx": "27.394827", "yy": "-26.3436425" }, "geometry": { "type": "Point", "coordinates": [ 27.394827, -26.3436425 ] } },
+{ "type": "Feature", "properties": { "Name": "Entrance 3\n", "descriptio": "1", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.324, "xx": "27.3845906", "yy": "-26.3241648" }, "geometry": { "type": "Point", "coordinates": [ 27.3845906, -26.3241648 ] } },
+{ "type": "Feature", "properties": { "Name": "Entrance 4\n", "descriptio": "1", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.351, "xx": "27.3615466", "yy": "-26.3507962" }, "geometry": { "type": "Point", "coordinates": [ 27.3615466, -26.3507962 ] } },
+{ "type": "Feature", "properties": { "Name": "Entrance 5\n", "descriptio": "2", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.348, "xx": "27.3725973", "yy": "-26.3483543" }, "geometry": { "type": "Point", "coordinates": [ 27.3725973, -26.3483543 ] } },
+{ "type": "Feature", "properties": { "Name": "Entrance 6\n", "descriptio": "2", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.365, "xx": "27.3788951", "yy": "-26.3652528" }, "geometry": { "type": "Point", "coordinates": [ 27.3788951, -26.3652528 ] } },
+{ "type": "Feature", "properties": { "Name": "Entrance 7\n", "descriptio": "1", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.384, "xx": "27.3623565", "yy": "-26.3841054" }, "geometry": { "type": "Point", "coordinates": [ 27.3623565, -26.3841054 ] } },
+{ "type": "Feature", "properties": { "Name": "Point 9", "descriptio": "1", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.376, "xx": "27.3979641", "yy": "-26.3762299" }, "geometry": { "type": "Point", "coordinates": [ 27.3979641, -26.3762299 ] } },
+{ "type": "Feature", "properties": { "Name": "Point 10", "descriptio": "1", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.374, "xx": "27.4065472", "yy": "-26.374346" }, "geometry": { "type": "Point", "coordinates": [ 27.4065472, -26.374346 ] } },
+{ "type": "Feature", "properties": { "Name": "Point 11", "descriptio": "2", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.361, "xx": "27.4137593", "yy": "-26.3612559" }, "geometry": { "type": "Point", "coordinates": [ 27.4137593, -26.3612559 ] } },
+{ "type": "Feature", "properties": { "Name": "Point 12", "descriptio": "1", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.371, "xx": "27.4192333", "yy": "-26.3713939" }, "geometry": { "type": "Point", "coordinates": [ 27.4192333, -26.3713939 ] } },
+{ "type": "Feature", "properties": { "Name": "Point 13", "descriptio": "3", "begin": null, "end": null, "icon": null, "x": "27", "y": -26.364, "xx": "27.3518562", "yy": "-26.3641844" }, "geometry": { "type": "Point", "coordinates": [ 27.3518562, -26.3641844 ] } }
+]
+}
